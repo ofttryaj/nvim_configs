4e9c00ca1e5239d20e46d4f6cfd57b8c185f9531
wanggang.erjh <wanggang.erjh@bytedance.com>
2025-02-06 14:43:41 +0800

    configure blink.cmp
    Change-Id: I93e219c9301402ea6b4913f3554dbcda226fb00d

---
+++ lua/plugins/blinkcmp.lua
@@ -0,0 +1,32 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      keyword = {
+        range = "full",
+      },
+      list = { selection = { preselect = false, auto_insert = true } },
+    },
+    keymap = {
+      preset = "none",
+
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      providers = {
+        buffer = { score_offset = 3 },
+        lsp = { score_offset = 2 },
+        path = { score_offset = 1 },
+        snippets = { score_offset = 0 },
+      },
+    },
+  },
+}
