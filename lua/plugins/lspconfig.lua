359dfd05e97ec848ad64110b4d9a1286dac604fc
wanggang.erjh <wanggang.erjh@bytedance.com>
2025-02-17 10:47:06 +0800

    update ui
    Change-Id: I5c42ef63ca846c63f041b8855e7643cf5fc54640

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,9 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = "LazyFile",
+  opts = {
+    inlay_hints = {
+      enabled = false,
+    },
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.sections.lualine_b = {}
+    end,
+  },
+}
