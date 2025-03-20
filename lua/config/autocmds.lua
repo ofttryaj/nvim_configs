34cfd4ce4c1ade532f3e33953c260a93adc97f30
wanggang.erjh <wanggang.erjh@bytedance.com>
2025-03-20 11:29:08 +0800

    autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("ofthoo_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("json_conceal"),
+  pattern = { "json", "jsonc", "json5" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+    vim.opt.shiftwidth = 2 -- Size of an indent
+    vim.opt.tabstop = 2 -- -- Number of spaces tabs count for
+    vim.opt.softtabstop = 2
+  end,
+})
